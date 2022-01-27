1bcb90405bd52152fd6e4c2ce5715c28a04b63a2
xgadrhh <hendrik.rohe@atruvia.de>
2022-01-27 11:52:05 +0100

    Kommentaraenderung nr2

--- org.eclipse.forkexample.calc/src/org/eclipse/example/calc/internal/Calculator.java
+++ org.eclipse.forkexample.calc/src/org/eclipse/example/calc/internal/Calculator.java
@@ -40,6 +40,7 @@ public class Calculator {
 	    // Das ist der zweite Kommentar in master
 	    // auch noch ein Kommentar von Ruth
 		// hallo hier ist ein Kommentar :)
+		// Kommentar nr2
 		new Equals();
 		new Minus();
 		new Plus();
